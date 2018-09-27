c66b5be7054769b5f20197389e2fa3842fdf8304
Angela Jia <42385351+22angiej@users.noreply.github.com>
2018-09-27 16:33:40 -0700

    Add Drivetrain class and two instance variables

--- src/main/java/frc/robot/subsystems/Drivetrain.kt
+++ src/main/java/frc/robot/subsystems/Drivetrain.kt
@@ -1,2 +1,11 @@
 package frc.robot.subsystems
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+
+
+
+object Drivetrain {
+    val leftTalon: TalonSRX? = null
+    val rightTalon: TalonSRX? = null
+
+}
