77127a62897b35bdaf41351de4de6f16a4cccc03
Finn Frankis <finn@teachmy.com>
2018-09-27 15:52:45 -0700

    Add all changes up to September 27th

---
+++ src/main/java/frc/robot/Robot.kt
@@ -0,0 +1,48 @@
+package frc.robot
+
+import edu.wpi.first.wpilibj.*
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+class Robot : TimedRobot() {
+
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    override fun robotInit() {}
+
+    /**
+     * This function is run once each time the robot enters autonomous mode.
+     */
+    override fun autonomousInit() {
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    override fun autonomousPeriodic() {
+    }
+
+    /**
+     * This function is called once each time the robot enters teleoperated mode.
+     */
+    override fun teleopInit() {
+    }
+
+    /**
+     * This function is called periodically during teleoperated mode.
+     */
+    override fun teleopPeriodic() {
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    override fun testPeriodic() {}
+}
