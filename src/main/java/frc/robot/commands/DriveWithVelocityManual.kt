c057931fa4f38e72d1e1220c402afe00dbffe0f8
Angela Jia <42385351+22angiej@users.noreply.github.com>
2018-10-01 09:14:50 -0700

    Include javadoc for DriveWithVelocityManual methods

--- src/main/java/frc/robot/commands/DriveWithVelocityManual.kt
+++ src/main/java/frc/robot/commands/DriveWithVelocityManual.kt
@@ -7,39 +7,54 @@ import frc.robot.OI
 import frc.robot.subsystems.Drivetrain
 import org.harker.robotics.harkerrobolib.wrappers.GamepadWrapper
 
-class DriveWithVelocityManual : Command(){
+class DriveWithVelocityManual : Command() {
     var speed = 0.0
     var turn = 0.0
+
     override fun end() {
 
     }
 
+    /**
+     * Sets the gamepad speed and turn output
+     * to the desired values to move the robot optimally.
+     */
     override fun execute() {
         speed = mapSpeed(OI.gamepad.leftY)
         turn = mapTurn(OI.gamepad.leftX)
         Drivetrain.arcadeDrivePercentOutput(speed, turn)
     }
 
-    private fun mapSpeed(givenSpeed : Double): Double {
-        //checks if gamepad speed output is between [-1, 1] and if so, sets speed to 0
-        if( Math.abs(givenSpeed) <= Global.DEADBAND) return 0.0
-        //map map output between [deadband, 1] and [-1, -deadband] to  [0, 1] and [-1, 0] respectively
-        if(givenSpeed > 0) return (givenSpeed-Global.DEADBAND)/(1-Global.DEADBAND)
-        if(givenSpeed < 0) return (givenSpeed+Global.DEADBAND)/(1-Global.DEADBAND)
+    /**
+     * Constrains gamepad speed output from [-deadband, deadband] to 0.
+     * Maps output between [deadband, 1] and [-1, -deadband]
+     * to  [0, 1] and [-1, 0] respectively.
+     */
+    private fun mapSpeed(givenSpeed: Double): Double {
+        if (Math.abs(givenSpeed) <= Global.DEADBAND) return 0.0
+        if (givenSpeed > 0) return (givenSpeed - Global.DEADBAND) / (1 - Global.DEADBAND)
+        else return (givenSpeed + Global.DEADBAND) / (1 - Global.DEADBAND)
     }
 
-    private fun mapTurn(givenTurn : Double) : Double {
-        //checks if gamepad speed output is between [-1, 1] and if so, sets speed to 0
-        if( Math.abs(givenTurn) <= Global.DEADBAND) return 0.0
-        //map map output between [deadband, 1] and [-1, -deadband] to  [0, 1] and [-1, 0] respectively
-        if(givenTurn > 0) return (givenTurn-Global.DEADBAND)/(1-Global.DEADBAND)
-        if(givenTurn < 0) return (givenTurn+Global.DEADBAND)/(1-Global.DEADBAND)
+    /**
+     * Constrains gamepad turn output from [-deadband, deadband] to 0.
+     * Maps output between [deadband, 1] and [-1, -deadband]
+     * to  [0, 1] and [-1, 0] respectively.
+     */
+    private fun mapTurn(givenTurn: Double): Double {
+        if (Math.abs(givenTurn) <= Global.DEADBAND) return 0.0
+        if (givenTurn > 0) return (givenTurn - Global.DEADBAND) / (1 - Global.DEADBAND)
+        else return (givenTurn + Global.DEADBAND) / (1 - Global.DEADBAND)
 
     }
 
     override fun initialize() {
 
     }
-    override fun isFinished() : Boolean = false
+
+    /**
+     * Determines that command never ends.
+     */
+    override fun isFinished(): Boolean = false
 
 }
