a38218f7a438ba9455a28c8fe96f1dc40c41a7db
Angela Jia <42385351+22angiej@users.noreply.github.com>
2018-10-26 20:59:06 -0700

    Correct if/else statement in IntakeOuttakeManual

--- src/main/java/frc/robot/commands/IntakeOuttakeManual.kt
+++ src/main/java/frc/robot/commands/IntakeOuttakeManual.kt
@@ -27,7 +27,7 @@ class IntakeOuttakeManual : IndefiniteCommand() {
                     if (Math.signum(rightOperatorJoystickInput).toInt() == OI.JOYSTICK_UP) Intake.IntakeDirection.OUT else Intake.IntakeDirection.IN)
         }
 
-        if (Math.abs(leftOperatorJoystickInput) <= OI.LOGITECH_DEADBAND || Math.abs(rightOperatorJoystickInput) <= OI.LOGITECH_DEADBAND) {
+        if (Math.abs(leftOperatorJoystickInput) <= OI.LOGITECH_DEADBAND && Math.abs(rightOperatorJoystickInput) <= OI.LOGITECH_DEADBAND) {
             val driverJoystickIntakeInput = OI.driverGamepad.leftTrigger //MathUtil.mapJoystickOutput(OI.driverGamepad.leftTrigger, OI.XBOX_DEADBAND)
             val driverJoystickOuttakeInput = OI.driverGamepad.rightTrigger //MathUtil.mapJoystickOutput(OI.driverGamepad.rightTrigger, OI.XBOX_DEADBAND)
             if (driverJoystickIntakeInput > driverJoystickOuttakeInput) {
