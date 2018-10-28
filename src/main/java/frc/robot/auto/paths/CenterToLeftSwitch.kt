19baffa9320cd8fc24112d9813806c84534f21b4
Finn Frankis <finn@teachmy.com>
2018-10-27 22:38:59 -0700

    Remove unnecessary generic type specification in switch path

--- src/main/java/frc/robot/auto/paths/CenterToLeftSwitch.kt
+++ src/main/java/frc/robot/auto/paths/CenterToLeftSwitch.kt
@@ -3,6 +3,6 @@ package frc.robot.auto.paths
 import harkerrobolib.auto.Path
 import jaci.pathfinder.Waypoint
 class CenterToLeftSwitch :
-        Path(arrayOf<Waypoint>(
+        Path(arrayOf(
                 Waypoint(1.64, 13.00, 0.0),
                 Waypoint(10.70,18.00, 20.00)))
