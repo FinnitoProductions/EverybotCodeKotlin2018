ab1f87606e503c6cfcafb731cf9ee84013a35b23
Finn Frankis <finn@teachmy.com>
2018-10-27 22:37:48 -0700

    Simplify center to left switch path generation

--- src/main/java/frc/robot/auto/paths/CenterToLeftSwitch.kt
+++ src/main/java/frc/robot/auto/paths/CenterToLeftSwitch.kt
@@ -2,6 +2,7 @@ package frc.robot.auto.paths
 
 import harkerrobolib.auto.Path
 import jaci.pathfinder.Waypoint
-class CenterToLeftSwitch : Path(arrayListOf<Waypoint>(Waypoint(1.64, 13.00, 0.0),Waypoint(10.70,18.00, 20.00)).toArray() as Array<out Waypoint>?) {
-
- }
+class CenterToLeftSwitch :
+        Path(arrayOf<Waypoint>(
+                Waypoint(1.64, 13.00, 0.0),
+                Waypoint(10.70,18.00, 20.00)))
