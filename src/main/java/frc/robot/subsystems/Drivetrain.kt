2fc16804441be6cff49864608981e9f924c1eac1
Angela Jia <42385351+22angiej@users.noreply.github.com>
2018-11-01 16:45:38 -0700

    Change Drivetrain to use HarkerRoboLib's HSDrivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.kt
+++ src/main/java/frc/robot/subsystems/Drivetrain.kt
@@ -4,9 +4,9 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import frc.robot.*
 import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.NeutralMode
-import edu.wpi.first.wpilibj.command.Subsystem
 import frc.robot.commands.ArcadeDriveVelocity
-import frc.robot.commands.TankDriveVelocity
+import harkerrobolib.subsystems.HSDrivetrain
+import harkerrobolib.wrappers.HSTalon
 
 /**
  * Represents the drivetrain on the robot with two master Talons and two follower Talons and functionality
@@ -16,11 +16,8 @@ import frc.robot.commands.TankDriveVelocity
  * @author  Finn Frankis
  * @version 9/27/18
  */
-object Drivetrain : Subsystem() {
-    val leftMaster: TalonSRX
-    val rightMaster: TalonSRX
-    val leftFollower: TalonSRX
-    val rightFollower: TalonSRX
+object Drivetrain : HSDrivetrain(HSTalon(CAN_IDs.LEFT_MASTER_ID), HSTalon(CAN_IDs.RIGHT_MASTER_ID) , HSTalon(CAN_IDs.LEFT_FOLLOWER_ID) , HSTalon(CAN_IDs.RIGHT_FOLLOWER_ID)) {
+
 
     //Talon constants
     const val LEFT_MASTER_INVERTED = true
@@ -28,10 +25,12 @@ object Drivetrain : Subsystem() {
     const val LEFT_FOLLOWER_INVERTED = true
     const val RIGHT_FOLLOWER_INVERTED = false
     val TALON_NEUTRAL_MODE = NeutralMode.Brake
-    const val TALON_PEAK_RIGHT_CURRENT = 50
-    const val TALON_PEAK_LEFT_CURRENT = 50
-    const val TALON_CONTINUOUS_RIGHT_CURRENT = 20
-    const val TALON_CONTINUOUS_LEFT_CURRENT = 20
+    //const val TALON_PEAK_RIGHT_CURRENT = 50
+    //const val TALON_PEAK_LEFT_CURRENT = 50
+    const val TALON_PEAK_CURRENT = 50
+    //const val TALON_CONTINUOUS_RIGHT_CURRENT = 20
+    //const val TALON_CONTINUOUS_LEFT_CURRENT = 20
+    const val TALON_CONTINUOUS_CURRENT = 20
     const val TALON_PEAK_TIME = 500
     const val TALON_CURRENT_ENABLE = true
 
@@ -56,50 +55,19 @@ object Drivetrain : Subsystem() {
 
     val DEFAULT_COMMAND = ArcadeDriveVelocity()
 
-
-    init {
-        leftMaster = TalonSRX(CAN_IDs.LEFT_MASTER_ID)
-        rightMaster = TalonSRX(CAN_IDs.RIGHT_MASTER_ID)
-        leftFollower = TalonSRX(CAN_IDs.LEFT_FOLLOWER_ID)
-        rightFollower = TalonSRX(CAN_IDs.RIGHT_FOLLOWER_ID)
-    }
-
     fun talonInit() {
         followMasters()
         invertTalons()
-        setNeutralModes()
-        setCurrentLimits()
+        setNeutralMode(TALON_NEUTRAL_MODE)
+        setCurrentLimit(TALON_PEAK_CURRENT, TALON_PEAK_TIME, TALON_CONTINUOUS_CURRENT)
         setUpPositionPID()
-    }
-
-    private fun followMasters() {
-        leftFollower.follow(leftMaster)
-        rightFollower.follow(rightMaster)
     }
 
     private fun invertTalons() {
         leftMaster.inverted = LEFT_MASTER_INVERTED
         rightMaster.inverted = RIGHT_MASTER_INVERTED
-        leftFollower.inverted = LEFT_FOLLOWER_INVERTED
-        rightFollower.inverted = RIGHT_FOLLOWER_INVERTED
-    }
-
-    private fun setNeutralModes() {
-        leftMaster.setNeutralMode(TALON_NEUTRAL_MODE)
-        rightMaster.setNeutralMode(TALON_NEUTRAL_MODE)
-        leftFollower.setNeutralMode(TALON_NEUTRAL_MODE)
-        rightFollower.setNeutralMode(TALON_NEUTRAL_MODE)
-    }
-
-    private fun setCurrentLimits() {
-        val applyCurrentLimit = {talon : TalonSRX, peakLimit : Int, contLimit : Int ->
-            talon.configPeakCurrentDuration(Drivetrain.TALON_PEAK_TIME, Global.TIMEOUT)
-            talon.configPeakCurrentLimit(peakLimit, Global.TIMEOUT)
-            talon.configContinuousCurrentLimit(contLimit, Global.TIMEOUT)
-            talon.enableCurrentLimit(Drivetrain.TALON_CURRENT_ENABLE)}
-
-        applyCurrentLimit.invoke(leftMaster, Drivetrain.TALON_PEAK_LEFT_CURRENT, Drivetrain.TALON_CONTINUOUS_LEFT_CURRENT)
-        applyCurrentLimit.invoke(rightMaster, Drivetrain.TALON_PEAK_RIGHT_CURRENT, Drivetrain.TALON_CONTINUOUS_RIGHT_CURRENT)
+        leftFollower!!.inverted = LEFT_FOLLOWER_INVERTED
+        rightFollower!!.inverted = RIGHT_FOLLOWER_INVERTED
     }
 
     fun arcadeDrivePercentOutput(speed: Double, turn: Double) {
@@ -125,6 +93,7 @@ object Drivetrain : Subsystem() {
             talon.config_kP(POSITION_PID_SLOT, kP, Global.TIMEOUT)
             talon.config_kI(POSITION_PID_SLOT, kI, Global.TIMEOUT)
             talon.config_kD(POSITION_PID_SLOT, kD, Global.TIMEOUT)}
+
         setupPID.invoke(leftMaster, Drivetrain.KF_POSITION_LEFT, Drivetrain.KP_POSITION_LEFT, Drivetrain.KI_POSITION_LEFT, Drivetrain.KD_POSITION_LEFT)
         setupPID.invoke(rightMaster, Drivetrain.KF_POSITION_RIGHT, Drivetrain.KP_POSITION_RIGHT, Drivetrain.KI_POSITION_RIGHT, Drivetrain.KD_POSITION_RIGHT)
     }
