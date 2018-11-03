3a3d8d898c21f018a5f8f53733516a7472aab0f1
Angela Jia <42385351+22angiej@users.noreply.github.com>
2018-11-03 13:56:27 -0700

    Configure soft limit for Arm

--- src/main/java/frc/robot/subsystems/Arm.kt
+++ src/main/java/frc/robot/subsystems/Arm.kt
@@ -24,18 +24,21 @@ object Arm : HSArm(HSTalon(CAN_IDs.ARM_TALON_ID), 0.05) {
     const val INVERTED = true
     val TALON_NEUTRAL_MODE = NeutralMode.Brake
 
-        const val TALON_PEAK_CURRENT = 17
-        const val TALON_CONTINUOUS_CURRENT = 10
-        const val TALON_PEAK_TIME = 1000
-        const val TALON_CURRENT_ENABLE = true
-        const val MAX_MANUAL_SPEED = 1.0
-        const val TALON_MOTION_DIRECTION = 1
-        const val TALON_CURRENT_SPIKE = 2.6
-        const val MAX_POSITION_UP_SPEED = 0.7
-        const val MAX_POSITION_DOWN_SPEED = 0.6
-        const val FEED_FORWARD_GRAV = 0.05
+    const val TALON_PEAK_CURRENT = 17
+    const val TALON_CONTINUOUS_CURRENT = 10
+    const val TALON_PEAK_TIME = 1000
+    const val TALON_CURRENT_ENABLE = true
+    const val MAX_MANUAL_SPEED = 1.0
+    const val TALON_MOTION_DIRECTION = 1
+    const val TALON_CURRENT_SPIKE = 2.6
+    const val MAX_POSITION_UP_SPEED = 0.7
+    const val MAX_POSITION_DOWN_SPEED = 0.6
+    const val FEED_FORWARD_GRAV = 0.05
     const val ENCODER_PHASE = false
     const val POSITION_PID_SLOT = 0
+    const val MAX_UP_POSITION = 1000
+    const val MAX_DOWN_POSITION = 0
+
 
 
 
@@ -55,6 +58,7 @@ object Arm : HSArm(HSTalon(CAN_IDs.ARM_TALON_ID), 0.05) {
         invertTalons()
         setNeutralModes()
         setCurrentLimits(TALON_PEAK_TIME, TALON_PEAK_CURRENT, TALON_CONTINUOUS_CURRENT)
+        configSoftLimit()
     }
 
     private fun invertTalons() {
@@ -65,5 +69,10 @@ object Arm : HSArm(HSTalon(CAN_IDs.ARM_TALON_ID), 0.05) {
         talon.setNeutralMode(TALON_NEUTRAL_MODE)
     }
 
-
+    private fun configSoftLimit() {
+        talon.configForwardSoftLimitThreshold(MAX_UP_POSITION)
+        talon.configForwardSoftLimitEnable(true)
+        talon.configReverseSoftLimitThreshold(MAX_DOWN_POSITION)
+        talon.configReverseSoftLimitEnable(true)
+    }
 }
