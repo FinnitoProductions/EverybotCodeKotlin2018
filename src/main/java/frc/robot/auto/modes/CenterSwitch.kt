d1580c090064591dea5b7ca0bf0ed468fef46301
Finn Frankis <finn@teachmy.com>
2018-11-03 13:26:54 -0700

    Create basic template for center switch auton

---
+++ src/main/java/frc/robot/auto/modes/CenterSwitch.kt
@@ -0,0 +1,9 @@
+package frc.robot.auto.modes
+
+import frc.robot.commands.DriveToPosition
+import harkerrobolib.auto.AutoMode
+import harkerrobolib.auto.SequentialCommandGroup
+
+class CenterSwitch(val endLoc : AutoMode.StartLocation ) : AutoMode(endLoc) {
+
+}
